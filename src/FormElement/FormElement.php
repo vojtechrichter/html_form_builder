9de64ffc404da07f67618aa2fcb8a5e79dd2f895
vojtechrichter <vojta3218@gmail.com>
2024-09-18 11:19:16 +0200

    Extended FormElement by tag value (inner text)

--- src/FormElement/FormElement.php
+++ src/FormElement/FormElement.php
@@ -8,14 +8,16 @@ abstract class FormElement
 {
     protected string $name;
     protected string $label;
+    protected string $tag_value;
 
     /** @var FormAttribute[] $attributes */
     protected array $attributes = [];
 
-    public function __construct(string $name, string $label = "")
+    public function __construct(string $name, string $label = "", string $tag_value = "")
     {
         $this->name = $name;
         $this->label = $label;
+        $this->tag_value = $tag_value;
     }
 
     public function addAttribute(FormAttribute $attribute): self
